1169f11269dd585fc7d5db90d9fcda08edb3bbfe
NotMatthewA <mattkirkanderson@gmail.com>
2018-11-09 10:33:22 -0600

    My first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function hideSidebar(){
+    document.getElementsByClassName("Sidebar")[0].className = "Sidebar Disabled-Sidebar"
+}
+function showSidebar(){
+    document.getElementsByClassName("Sidebar")[0].className = "Sidebar Enabled-Sidebar"
+}
