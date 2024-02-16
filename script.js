fd174c833119b2fb1588b2abd6d8d24aea24c1af
Matt Anderson <matthew.anderson@capspire.com>
2024-02-16 11:18:46 -0600

    init commit

--- script.js
+++
@@ -1,6 +0,0 @@
-function hideSidebar(){
-    document.getElementsByClassName("Sidebar")[0].className = "Sidebar Disabled-Sidebar"
-}
-function showSidebar(){
-    document.getElementsByClassName("Sidebar")[0].className = "Sidebar Enabled-Sidebar"
-}
